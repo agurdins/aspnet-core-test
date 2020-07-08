dca047a3a1bc12c732d46212b9c28b5b8be514cd
Deivids Agriņš <Deivids.Agrins@edu.rtu.lv>
2020-07-08 19:24:58 +0300

    Added person controller

---
+++ ASPNetCore/ASPNetCore/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using ASPNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var person = new Person();
+            person.Name = "Deivids";
+            person.Surname = "Agriņš";
+            person.Age = 22;
+            person.Email = "deivids.agrins@gmail.com";
+            person.IsMale = true;
+            person.Adress = "Šarlotes tante";
+
+            return View(person);
+        }
+    }
+}
---
+++ ASPNetCore/ASPNetCore/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Adress { get; set; }
+        public bool IsMale { get; set; }
+
+    }
+}
